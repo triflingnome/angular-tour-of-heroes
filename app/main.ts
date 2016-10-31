ac21a8e702fda21d9f9b1a3e03e48c4c8d408716
Michael Berg <michael.rooney.berg@gmail.com>
2016-10-31 11:47:43 -0500

    Added comments

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+// tutorial link here: https://angular.io/docs/ts/latest/tutorial/toh-pt6.html
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
